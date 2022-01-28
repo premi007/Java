641ca1e11832dfed64f21aa603f488ad6bb8f82d
premi007 <87196212+premi007@users.noreply.github.com>
2022-01-28 18:57:34 +0000

    Update MultiplesOf2.java

--- Java Day 4 Asssessment/MultiplesOf2.java
+++ Java Day 4 Asssessment/MultiplesOf2.java
@@ -3,14 +3,14 @@ public class MultiplesOf2
 {
 	public static void main(String args[])
 	{
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i = 1; i <= n; i++)
-        {
-            if(i % 2 == 0)
-            {
-                System.out.print(i + " ");
-            }
-        }
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 1; i <= n; i++)
+		{
+		    if(i % 2 == 0)
+		    {
+			System.out.print(i + " ");
+		    }
+		}
 	}
 }
