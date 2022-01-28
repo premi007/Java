743961d3d9e14f978306f3711740f9ec5055ac04
premi007 <87196212+premi007@users.noreply.github.com>
2022-01-28 17:28:57 +0000

    Update NNumbers.java

--- Java Day 4 Assessment/NNumbers.java
+++ Java Day 4 Assessment/NNumbers.java
@@ -6,7 +6,7 @@ public class NNumbers
         int n = sc.nextInt();
         for(int i = 1; i <= n; i++)
         {
-          System.out.print(i);
+          System.out.print(i + " ");
         } 
   }
 }  
